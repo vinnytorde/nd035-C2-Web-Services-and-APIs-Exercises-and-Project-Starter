3d71167de025da9e4ab6f3429bd2ccfa842a79b6
vinny <vinnytorde@gmail.com>
2021-04-08 08:57:27 -0500

    refactor controller to spring data uri format

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
@@ -4,17 +4,14 @@ import com.udacity.pricing.domain.price.Price;
 import com.udacity.pricing.service.PriceException;
 import com.udacity.pricing.service.PricingService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Implements a REST-based controller for the pricing service.
  */
 @RestController
-@RequestMapping("/services/price")
+@RequestMapping
 public class PricingController {
 
     /**
@@ -22,8 +19,8 @@ public class PricingController {
      * @param vehicleId ID number of the vehicle for which the price is requested
      * @return price of the vehicle, or error that it was not found.
      */
-    @GetMapping
-    public Price get(@RequestParam Long vehicleId) {
+    @GetMapping("/prices/{vehicleId}")
+    public Price get(@PathVariable(value = "vehicleId") Long vehicleId) {
         try {
             return PricingService.getPrice(vehicleId);
         } catch (PriceException ex) {
